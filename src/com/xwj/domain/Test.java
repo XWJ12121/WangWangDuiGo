41d4abfe2c6a0fd41be4090c69321c6d60e71de8
xwj <441417382@qq.com>
2021-05-26 15:39:44 +0800

    9

--- src/com/xwj/domain/Test.java
+++ src/com/xwj/domain/Test.java
@@ -2,6 +2,6 @@ package com.xwj.domain;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("nihaoyaya");
+        System.out.println("nihaoyaya1427");
     }
 }
